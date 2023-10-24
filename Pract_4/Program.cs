deccd04cf88395ef73d0ecf26134d70188fb7a42
Dmitry <dima2323fr3@gmail.com>
2023-10-24 15:06:53 +0700

    first chane

---
+++ Pract_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Pract_4;
+
+string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+int cntSymbols = 5;
+var monthsBySymbols = from symbol in months where symbol.Length == cntSymbols select symbol;
+foreach (var l in monthsBySymbols) Console.WriteLine(l);
+Console.WriteLine();
+
+var monthsBySeasons = from character in months where (character.StartsWith('J') || character.StartsWith('F') || character.StartsWith('D') || character.StartsWith("Au")) select character;
+foreach (var c in monthsBySeasons) Console.WriteLine(c);
+Console.WriteLine();
+
+var monthsInAlphOrder = from alph in months orderby alph select alph;
+foreach (var a in monthsInAlphOrder) Console.WriteLine(a);
+Console.WriteLine();
+
+var monthsWithU = from uChar in months where (uChar.Contains("u") && uChar.Length >= 4) select uChar;
+foreach (var u in monthsWithU) Console.WriteLine(u);
+Console.WriteLine();
+
+Time clock_1 = new Time(1,24,0);
+clock_1.PrintTime();
+Time clock_2 = new Time(0, 0, 10000);
+clock_2.PrintTime();
---
+++ Pract_4/Time.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Pract_4
+{
+    public class Time
+    {
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        List<int> objecList = new List<int>();
+
+        int Seconds
+        {
+            get => seconds;
+            set
+            {
+                int vSec = seconds + value;
+                seconds = Math.Abs((vSec) % 60);
+                if (vSec % 60 == 0 || vSec > 60)
+                {
+                    Minutes = (vSec / 60);
+                }
+            }
+        }
+        int Minutes
+        {
+            get => minutes;
+            set
+            {
+                int vMin = minutes + value;
+                minutes = Math.Abs((vMin) % 60);
+                if (vMin % 60 == 0 || vMin > 60)
+                {
+                    Hours = vMin / 60;
+                }
+
+            }
+        }
+        int Hours
+        {
+            get => hours;
+            set => hours = Math.Abs((hours + value) % 24);
+            
+        }
+
+        
+
+        
+
+        public Time() {; }
+
+        public Time(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        private void AddToList()
+        {
+            objecList.Clear();
+            objecList.Add(hours);
+            objecList.Add(minutes);
+            objecList.Add(seconds);
+        }
+        private void CheckTime(params int[] status)
+        {
+            AddToList();
+            string time = null;
+            for(int i = 0; i < status.Length; i++)
+                time += status[i]==-1 ? ("0" + objecList[i]+":") : (objecList[i]+":");
+            time=time.TrimEnd(':');
+            Console.WriteLine(time);
+        }
+        public void PrintTime()
+        {
+            int[] status = {0,0,0};
+            status[0] = Hours < 10 ? -1 : 1;
+            status[1] = Minutes < 10 ? -1 : 1;
+            status[2] = Seconds < 10 ? -1 : 1;
+            CheckTime(status);
+        }
+
+    }
+}
